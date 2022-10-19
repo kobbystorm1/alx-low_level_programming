67ac61ac6c9cefb7e3049ebff38a912c392f6ea2
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-19 15:51:41 -0700

    C files housekeeping

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main(void) /*The entry point */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

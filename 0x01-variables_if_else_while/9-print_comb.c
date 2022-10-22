ed68a82be5050781266d11e1b6f3f2acefbc0c0a
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-22 16:59:47 -0700

    Added a counting numbers with commas and soaces program

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+	char c = 'a';
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

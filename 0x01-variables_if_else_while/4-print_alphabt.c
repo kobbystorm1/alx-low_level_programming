3339c1846136f8b555e9591f0d83bd6065a6afb1
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-22 10:25:54 -0700

    Added a alphabet with omission program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch = 'a';
+	char up = 'A';
+
+	while (ch <= 'z')
+	{
+		if ((ch == 'q') || (ch == 'e'))
+		{
+			ch++;
+			continue;
+		}
+		putchar (ch);
+		ch++;
+	}
+	putchar ('\n');
+
+	return (0);
+}

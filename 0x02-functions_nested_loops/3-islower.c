5d9fbf9c3d4eca71fdbab3e219cdc328468db935
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-26 11:31:23 -0700

    added a checker for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - checks if character is a lower character
+ *
+ * Return: 0.
+ * c - placeholder for the character
+ */
+int _islower(int c);/*c - character to be held here*/
+
+{
+	int c;/*character to be held here*/
+
+	if ((c >= 'a') && (c <= 'z'))
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}

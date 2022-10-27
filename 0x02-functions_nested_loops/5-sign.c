3f357a5cbb1ebc7f351e6680481c2c7db34fe4a9
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-27 09:56:05 -0700

    Added a number sign checker

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,29 @@
 #include "main.h"
 /**
  * print_sign - prints sign of number
- *  @n: Placeholder for the number
- *
+ * @n: Placeholder for the number
+ * @m: Placeholder for return variable
+ * 
  * Return: 1, 0,-1
  */
 
 int print_sign(int n)
 {
+	int m;
+
 	if (n > 0)
 
 	{
-		_putchar(43);
-		_putchar(49);
+		m=+1;
 	}
 	else if (n == 0)
 	{
-		return (0);
+		m=0;
 	}
 	else
 	{
-		return (-1);
+		m=-1;
 	}
+	return m;
 
 }

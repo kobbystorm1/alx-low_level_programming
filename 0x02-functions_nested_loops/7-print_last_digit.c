b6cd63d1f25971315739cfe9aaa396f6fb02d543
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-27 12:55:04 -0700

    Added a last digit display program

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,19 @@
 
 int print_last_digit(int n)
 {
-
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	else
+	(
+		n = n; 
+	)
 	while (n > 10)
 
 	{
-		n = n%10;
+		n = n % 10;
 	}
-
+	_putchar(n + '0');
 	return (n);
 }

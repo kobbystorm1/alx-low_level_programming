dfbb436158a2fb2ab69761eb0c4c171da18678e7
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-27 12:00:43 -0700

    Added a sign detector

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,6 @@
 /**
  * print_sign - prints sign of number
  * @n: Placeholder for the number
- * @m: Placeholder for return variable
  *
  * Return: 1, 0,-1
  */
@@ -14,19 +13,19 @@ int print_sign(int n)
 
 	{
 		_putchar(43);
-		_putchar(49);
+		/*_putchar(49);*/
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-		_putchar(48);
+		/*_putchar(48);*/
 		return (0);
 	}
 	else
 	{
 		_putchar(45);
-		_putchar(49);
+		/*_putchar(49);*/
 		return (-1);
 	}
 }

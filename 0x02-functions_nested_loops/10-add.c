de92522c88d214bcc946fe2a3c5891607ead26a3
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-30 11:45:55 -0700

    Added an integer addition program using putchar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * add - prints result of 2 integer addition
+ * @a: Placeholder for integer1
+ * @b: Placeholder for integer2
+ *
+ * Return: integer sum
+ */
+
+int add(int a, int b)
+{
+	int ans;
+
+	ans = a + b;
+
+	if (ans > 9)
+	{
+	_putchar((ans / 10) + '0');
+	_putchar((ans % 10) + '0');
+	}
+	else
+	{
+	_putchar(ans  + '0');
+	}
+
+	return (0);
+}

48854ff21846f9f7835e09b6d25c0a407166dfab
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-30 19:01:39 -0700

    Added a countdown to 98 program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -43,6 +43,22 @@ void print_to_98(int n)
 			}
 		}
 	}
+	else if (n < 0)
+	{
+		while (n < 0)
+		{
+			n = (-1) * n;
+			for (x = n; x < 0; x--)
+			{
+				_putchar('-');
+				if (x > 99)
+				{
+                        {                                                                                                  _putchar((x / 100) + '0');                                                                 y = (x / 10);                                                                                                                                                                         if ((x % 100) >= 10)                                                                       {                                                                                                  y = (y / 10);                                                                              _putchar(y + '0');                                                                 }                                                                                          else                                                                                       {                                                                                                  _putchar((y % 10) + '0');                                                          }                                                                                          _putchar((x % 10) + '0');                                                                  _putchar(',');                                                                             _putchar(' ');                                                                     }                                                                                          else                                                                                       {                                                                                                  _putchar((x / 10) + '0');                                                                  _putchar((x % 10) + '0');                                                                  if (x != 98)                                                                               {                                                                                                  _putchar(',');                                                                             _putchar(' ');                                                                     }                                                         
+				
+
+			}
+		}		
+	}
 	else
 	{
 		for (x = n; x < 99; x++)

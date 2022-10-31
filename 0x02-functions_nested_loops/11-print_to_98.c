5aef02fc3aa694a719230d742eb1f0ee640d7749
kobbystorm1 <kobbystorm1@gmail.com>
2022-10-30 19:46:10 -0700

    Added a countdown to 98 program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -45,25 +45,60 @@ void print_to_98(int n)
 	}
 	else if (n < 0)
 	{
-		while (n < 0)
+		for (x = ((-1) * n); x > 0; x--)
 		{
-			n = (-1) * n;
-			for (x = n; x < 0; x--)
+			_putchar('-');
+			if (x > 99)
 			{
-				_putchar('-');
-				if (x > 99)
+				_putchar((x / 100) + '0');
+				y = (x / 10);
+				if ((x % 100) >= 10)
 				{
-                        {                                                                                                  _putchar((x / 100) + '0');                                                                 y = (x / 10);                                                                                                                                                                         if ((x % 100) >= 10)                                                                       {                                                                                                  y = (y / 10);                                                                              _putchar(y + '0');                                                                 }                                                                                          else                                                                                       {                                                                                                  _putchar((y % 10) + '0');                                                          }                                                                                          _putchar((x % 10) + '0');                                                                  _putchar(',');                                                                             _putchar(' ');                                                                     }                                                                                          else                                                                                       {                                                                                                  _putchar((x / 10) + '0');                                                                  _putchar((x % 10) + '0');                                                                  if (x != 98)                                                                               {                                                                                                  _putchar(',');                                                                             _putchar(' ');                                                                     }                                                         
-				
-
+					y = (y / 10);
+					_putchar(y + '0');
+				}
+				else
+				{
+					_putchar((y % 10) + '0');
+				}
+				_putchar((x % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
-		}		
+			else
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		
+		for (x = 0; x < 99; x++)
+		{
+			if (x > 9)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				if (x < 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(x + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 	}
 	else
 	{
 		for (x = n; x < 99; x++)
 		{
-			if ((x > 9) || (x < -9))
+			if (x > 9)
 			{
 				_putchar((x / 10) + '0');
 				_putchar((x % 10) + '0');
